8cc18b390c82a1c06b3834267a9543ebb4e3caf6
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-01-26 20:34:56 -0600

    Giving it a go - mostly from memory here.

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,1 +1,21 @@
-#This is checksplitter
+require "pry"
+
+class Checksplitter
+  def initialize (billwtax, totalbill)
+    @persons = nil
+    @billwtax = billwtax.to_f
+    @bill = totalbill.to_f
+    @percent = nil 
+  end
+  
+  def persons
+    @persons
+  end
+  
+  def percent
+    @percent
+  end
+  
+  
+  
+    
