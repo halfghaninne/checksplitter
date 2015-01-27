cdb82a858c331e79199ccbe17f029dd63408d48e
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-01-27 11:03:22 -0600

    Created file for the Dinner Club assignment for in-class coding.

---
+++ dinner-club-checksplitter.rb
@@ -0,0 +1,47 @@
+require "pry"
+
+class Checksplitter
+  def initialize(bill_w_tax, subtotal, persons, percent)
+    @persons = persons
+    @bill_w_tax = bill_w_tax
+    @subtotal = subtotal
+    @percent = percent 
+  end
+  
+  def persons
+    @persons
+  end
+  
+  def percent
+    @percent * 0.01
+  end
+  
+  def bill_w_tax
+    @bill_w_tax
+  end
+  
+  def subtotal
+    @subtotal
+  end
+  
+  def sub_per_person
+    subtotal / persons
+  end
+  
+  def share_of_tax
+    (bill_w_tax - subtotal) / persons
+  end
+  
+  def solution
+    sub_per_person + (percent * sub_per_person) + share_of_tax
+  end
+
+end
+
+
+
+binding.pry
+
+  
+  
+    
