a2ca393aef549bfae1c614db1ff72b03ec65ce64
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-01-27 13:45:38 -0600

    In-class dinner club assignment as of "code review" in the afternoon.

--- dinner-club-checksplitter.rb
+++ dinner-club-checksplitter.rb
@@ -1,5 +1,33 @@
 require "pry"
 
+#SCRATCH WORK YO
+
+DinnerClub = {Hamza: bal1, Khalil: bal2, Selima: bal3, Alex: bal4}
+
+=begin
+class DinnerClubTotals
+  def initialize(event)
+    @event = event
+    @members = []
+    add_to_total("Beginning Balance", 0)
+  end
+  
+  def add_to_total(name, amount)
+    @
+  end
+  
+end
+=end
+
+#SCRATCH WORK YO
+
+class DinnerClubTotals
+
+end
+
+#SCRATCH WORK YO
+  
+  
 class Checksplitter
   def initialize(bill_w_tax, subtotal, persons, percent)
     @persons = persons
@@ -35,6 +63,20 @@ class Checksplitter
   def solution
     sub_per_person + (percent * sub_per_person) + share_of_tax
   end
+
+#Scratch additions
+
+def members_present(name1, name2, name3)
+    @memberspresent= Array.new
+end
+
+
+def assign_balances
+  @members = {}
+  @members[members_present] = solution
+  print @members
+end
+  
 
 end
 
