2ca8a654004fbc859449016e3a1124bbd6081583
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-01-26 21:34:14 -0600

    Rough cut for Checksplitter app. Does what it needs to do, though it could be finessed. If there is time tomorrow am, can go back and check work / tweak.

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,11 +1,11 @@
 require "pry"
 
 class Checksplitter
-  def initialize (billwtax, totalbill)
-    @persons = nil
+  def initialize(billwtax, subtotal, persons, percent)
+    @persons = persons
     @billwtax = billwtax
-    @bill = totalbill
-    @percent = 20 
+    @subtotal = subtotal
+    @percent = percent 
   end
   
   def persons
@@ -13,9 +13,28 @@ class Checksplitter
   end
   
   def percent
-    @percent
+    @percent * 0.01
   end
+  
+  def billwtax
+    @billwtax
+  end
+  
+  def subtotal
+    @subtotal
+  end
+  
+  def solution
+    sub = (subtotal) / persons
+    sharetax = (billwtax - subtotal) / persons
+    total = sub + (percent * sub) + sharetax
+    puts total
+  end
+
 end
+
+new = Checksplitter.new(150, 108, 3, 20)
+puts new.billwtax
 
 binding.pry
 
